3dfe2cb430b22bc38d323df912a8bd98c880248b
sehzad23 <sehaans87@gmail.com>
2026-07-14 13:45:31 +0530

    Update CORS origin in socket server to use environment variable

--- Backend/src/sockets/socket.server.js
+++ Backend/src/sockets/socket.server.js
@@ -11,7 +11,7 @@ const { createMemory, queryMemory } = require("../services/vactor.service");
 function initSocketServer(httpServer) {
   const io = new Server(httpServer, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: process.env.ORIGIN,
       credentials: true,
     },
   });
